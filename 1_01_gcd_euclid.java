a4ccc8f8f90b853a4f09916b90aac5e9e36f8ae5
Caroline-Wendy <morndragon@126.com>
2013-11-28 21:42:51 +0800

    1_01

---
+++ 1_01_gcd_euclid.java
@@ -0,0 +1,24 @@
+/*
+ * algorithms.java
+ *
+ *  Created on: 2013.11.28
+ *      Author: Spike
+ */
+
+/*eclipse kepler, javase-1.7*/
+
+public class algorithms {
+	//最大公约数
+	public static int gcd (int p, int q)
+	{
+		if (q == 0) return p;
+		int r = p % q;
+		return gcd(q, r);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int res = gcd(42, 70);
+		System.out.println("Hei, the gcd is " + res + ". ");
+	}
+}
